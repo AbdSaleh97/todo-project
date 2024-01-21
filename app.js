5941c25a3d7a98a8ec3d7280ea71dc73903edb09
AbdSaleh97 <salehabd.897@gmail.com>
2024-01-21 18:33:34 +0300

    declaration error, check it later

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
 'use strict';
 
-let q1 = prompt("Is it night now?", "Yes/No?");
+let q1 = prompt("Is it night now?");
 let q2 = prompt("IS the moon full?");
 let q3 = prompt("DO you like full moons?");
 
-quesChecker();
+let tempArr = [q1, q2, q3];
 
-let tempArr = [];
-tempArr.push(q1);
-tempArr.push(q2);
-tempArr.push(q3);
+quesChecker();
 
 printArr(tempArr);
 
 function quesChecker() {
-    if (q1 === null || q1.trim() === "") {
-        q1 = "Invalid";
-    }
-    if (q2 === null || q2.trim() === "") {
-        q2 = "Invalid";
-    }
-    if (q3 === null || q3.trim() === "") {
-        q3 = "Invalid";
+
+    for (let index = 0; index < tempArr.length; index++) {
+        if (tempArr[index] === null || tempArr[index].trim() === "") {
+            tempArr[index] = "Invalid";
+        }
     }
 }
 
-function printArr(tempArr)
-{
-    for (let index = 0; index < tempArr.length; index++) {   
-        console.log(tempArr[index]); 
+function printArr(tempArr) {
+    for (let index = 0; index < tempArr.length; index++) {
+        console.log(tempArr[index]);
     }
 }
